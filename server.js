ebc721321b00c7602dc447a80537d24f35968d0d
Amir Sejdinovic <amir24V@yahoo.com>
2020-02-19 13:12:46 +0100

    Adding static files and connecting them with the express

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const express = require('express');
 //create new instance of express
 const app = express();
 
+//Telling the express that we are using static folder in whic will be static files
+//Whit this the index.html inside public folder will be automaticly served as home page when we visit the listen port
+app.use(express.static('public'));
+
 //Create that app listen the port
 app.listen(5001, ()=>{
   console.log('Listening to port 5001');
-})
+});
