04dde2651cddb6d10f48db2312426eb8b589a33a
Amir Sejdinovic <amir24V@yahoo.com>
2020-02-20 09:57:11 +0100

    Instaling the body parser npm package and importing it

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 //Importing the mustache template engine 
 const mustacheExpress = require('mustache-express');
+//Importing the body parser
+const bodyParser = require('body-parser');
 
 //create new instance of express
 const app = express();
@@ -18,14 +20,26 @@ app.set('view engine', 'mustache');
 //Whit this the index.html inside public folder will be automaticly served as home page when we visit the listen port
 app.use(express.static('public'));
 
+//Teling the expres that we use body parser package
+app.use(bodyParser.urlencoded({extended:false}));
+
 //Creating the route for meds and rendering the template 
 app.get('/meds',(req,res)=>{
   //Rednering the mustache template from views folder
     res.render('meds');
 } );
 
-//Rout from form
-
+//Rout from form 
+app.get('/add', (req,res)=>{
+  //Rendering the mustache template for form
+   res.render('med-form');
+});
+//Creating the route for post action from form
+app.post('/meds/add', (req,res)=>{
+  console.log('post body', req.body);
+  //When user hit the submit button on the form redirect them to the meds page
+    res.redirect('/meds');
+})
 
 //Create that app listen the port
 app.listen(5001, ()=>{
