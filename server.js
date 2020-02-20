3dccbd8789df6fce9949bb76b022b3a257ba3a5e
Amir Sejdinovic <amir24V@yahoo.com>
2020-02-20 10:21:02 +0100

    Inserting values from form into database

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 //Importing the body parser
 const bodyParser = require('body-parser');
+//Importin pg package (postgres package)
+const { Client } = require('pg');
+
+//
 
 //create new instance of express
 const app = express();
 //Create new instance of mustache template engine
 const mustache = mustacheExpress();
 
+
 mustache.cache = null;
 //Seting the template engine
 app.engine('mustache', mustache);
 //Seting the mustache as view engine
 app.set('view engine', 'mustache');
+
+
 
 //Telling the express that we are using static folder in whic will be static files
 //Whit this the index.html inside public folder will be automaticly served as home page when we visit the listen port
@@ -37,8 +44,34 @@ app.get('/add', (req,res)=>{
 //Creating the route for post action from form
 app.post('/meds/add', (req,res)=>{
   console.log('post body', req.body);
-  //When user hit the submit button on the form redirect them to the meds page
-    res.redirect('/meds');
+  //Creating new instance of Client or pg package which will hellp me to establish the connection with postgresSQL database
+  const client = new Client({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'medical1',
+      password: 'root',
+      port: 5432,
+
+  });
+  //Conecting to the database with this aboce credentials. In this i put the promise
+  client.connect()
+  .then(()=>{
+    //Consloe log the complete connection with database
+      console.log('Connection comlete');
+      //Creating const variable for sql query which will insert the date into db
+      const sql = "INSERT INTO meds (name,count,brand) VALUES($1,$2,$3)";
+      //Creating cons variable with values from form
+      const params = [req.body.name,req.body.count,req.body.brand];
+      //Connecting with database and inserting sql and params
+      return client.query(sql, params);
+  }).then((result)=>{
+    //Display results of insertin
+     console.log('results', result);
+     //When user hit the submit button on the form redirect them to the meds page
+      res.redirect('/meds');
+  })
+  ;
+  
 })
 
 //Create that app listen the port
